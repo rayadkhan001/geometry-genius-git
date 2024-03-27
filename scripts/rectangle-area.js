86cf89b72b8ffe6500276c20073c293c56400369
Rayad Hassin Khan Rahin <129431179+rayadkhan001@users.noreply.github.com>
2024-03-28 03:00:47 +0600

    added Parallelogram and finished rectangle

--- scripts/rectangle-area.js
+++ scripts/rectangle-area.js
@@ -1,3 +1,21 @@
+// the value of input field is always on text by default not number
 function calculateRectangleArea(){
-    console.log('calculate rectangle area');
+    // get length of the rectangle
+    const lengthInput = document.getElementById('rectangle-length');
+    const lengthText = lengthInput.value; // value was text by default so 
+    const length = parseFloat(lengthText); // I have converted it 
+    console.log(length);
+
+    // get width of the rectangle
+    const widthInput = document.getElementById('rectangle-width');
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+    console.log(width);
+
+    const area = length * width;
+    console.log('area of the rectangle:', area);
+
+    // display rectangle area 
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 }
